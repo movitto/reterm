76e88d7a337e07f87cdc44cab9888565796cb1a1
Mo Morsi <mo@morsi.org>
2018-01-06 07:45:48 -0500

    Stub out isometric renderer component

---
+++ lib/reterm/components/isometric.rb
@@ -0,0 +1,37 @@
+module RETerm
+  module Components
+    # Renders specified isometric grid
+    class Isometric < Component
+
+      # Initialize the Isometric component
+      #
+      # @param [Hash] args isometric params
+      # @option args [Array<Array<Integer,Double>>] :matrix 2-dimensional array
+      #   containing Z values corresponding to x/y grid coordinates
+      def initialize(args={})
+        @matrix = args[:matrix] || [[]]
+
+        raise ArgumentError unless @matrix.is_a?(Array) &&
+                                   @matrix.all? { |m| m.is_a?(Array) }
+      end
+
+      def requested_rows
+      end
+
+      def requested_cols
+      end
+
+      def draw!
+        refresh_win
+      end
+
+      private
+
+      def refresh_win
+        # ... TODO
+        window.refresh
+        update_reterm
+      end
+    end # class Isometric
+  end # module Components
+end # module RETerm
