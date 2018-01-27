75cfdf1ad81dc8de3270a5aa1895fb8855b45fbc
Mo Morsi <mo@morsi.org>
2018-01-27 08:50:48 -0500

    Remove common Layout methods now included in NavControls mixin

--- lib/reterm/layout.rb
+++ lib/reterm/layout.rb
@@ -197,20 +197,11 @@ module RETerm
     #
     # @see NavInput
     def activate!(*input)
-      @deactivate = false
+      reactivate!
 
       draw!
       update_reterm
       handle_input(*input)
-    end
-
-    # Decactivates layout by disabling further input
-    def deactivate!
-      @deactivate = true
-    end
-
-    def deactivate?
-      !!@deactivate
     end
   end # class Layout
 end # module RETerm
